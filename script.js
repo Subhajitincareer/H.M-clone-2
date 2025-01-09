e74315e92b1bc8c64ff376b6c99c1e6c3a4fc649
w3schools-test <subhajitincareer@gmail.com>
2025-01-09 19:52:54 +0530

    phase 2 complete

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  window.onload= Alert();
+
+  function Alert() {
+    alert("This website is a clone created solely for educational purposes.it is intended to  demonstrate and enhance web development skills and has no commercial intent. All credit for orginal design and concept go to H.M .  This project does not intend to infringe on any copyright or intellectual  property  rights .")
+  }
